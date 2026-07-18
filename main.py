1950d4c23ba65a1301c7bccdda4166136f143c4a
guooo5 <guo.chen.1@stonybrook.edu>
2026-07-18 19:31:25 -0400

    feat: added card flip after 3 seconds, added known_card dict to avoid showing same cards again

--- main.py
+++ main.py
@@ -10,29 +10,49 @@ data = pandas.read_csv("data/common_chinese_words.csv")
 data_dict = defaultdict(list)
 for row in data.itertuples():
     data_dict[row.Chinese] = row.English 
-    
-word_bank = set() #words already know
 
+word = random.choice(list(data_dict.keys())) 
 def change_text():
-    word = random.choice(list(data_dict.keys())) 
-    while word in word_bank:
-        word = random.choice(list(data_dict.keys())) 
-        
-    flash_card.itemconfig(flash_card_word, text=word)
+    global word, flip_timer 
+    screen.after_cancel(flip_timer)
     
+    word = random.choice(list(data_dict.keys()))     
+    flash_card.itemconfig(card_title, text="Chinese", fill="black")
+    flash_card.itemconfig(flash_card_word, text=word, fill="black")
+    flash_card.itemconfig(flash_card_image, image=card_front)
+    
+    flip_timer = screen.after(3000, func=flip_card)
+
+def flip_card():
+    flash_card.itemconfig(card_title, text="English", fill="white")
+    flash_card.itemconfig(flash_card_word, text=data_dict[word], fill="white")
+    flash_card.itemconfig(flash_card_image, image=card_back)
+
+def known_card():
+    del data_dict[word]
+    change_text()
+    print(len(data_dict))
+        
+    
+# def flip_card():
+#     flash_card.itemconfig(flash_card_image, image=card_back)
+#     flash_card.itemconfig(flash_card_word,text=data_dict[word])
 #---gui 
 #screen 
 screen = Tk()
 screen.title("Chinese Words Flash Card")
 screen.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = screen.after(3000, func=flip_card) #timer after 3 secs flip
+
 #card body
 flash_card = Canvas(width=800, height=526)
 card_front = PhotoImage(file="images/card_front.png")
-flash_card.create_image(400, 263, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+flash_card_image = flash_card.create_image(400, 263, image=card_front)
 
 #text
-flash_card.create_text(400, 150, text="Chinese", font=("Ariel", 40, "italic"), fill="black")
+card_title = flash_card.create_text(400, 150, text="Chinese", font=("Ariel", 40, "italic"), fill="black")
 
 flash_card_word = flash_card.create_text(400, 263, text="word", font=("Ariel", 60, "bold"), fill="black")
 
@@ -46,11 +66,8 @@ x_button = Button(image=x_image, highlightthickness=0, command=change_text)
 x_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-check_button = Button(image=check_image, highlightthickness=0, command=change_text)
+check_button = Button(image=check_image, highlightthickness=0, command=known_card)
 check_button.grid(row=1, column=1)
-
-
-
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
 
-    data_dict[row.Chinese] = row.English 
+    
